2f8cc1d1a3d2e5ee298555f062a17a565b6a391a
shail611 <85347670+shail611@users.noreply.github.com>
2024-12-18 00:39:16 -0500

    Update explore.py

--- transfer/explore.py
+++ transfer/explore.py
@@ -4,13 +4,13 @@ import pandas as pd
 # with open('transfer/features_v1.pkl', 'rb') as file:
 #     data = pickle.load(file)
 
-with open('transfer/features_v1.pkl', 'rb') as file:
+with open('transfer/subtype_v1.pkl', 'rb') as file:
     data = pickle.load(file)
 
 # Explore the data
 print(type(data))  # Check the type of the loaded data
-print("Shape of DataFrame:", data.shape)  # (rows, columns)
-print("Size of DataFrame:", data.size)    # Total number of elements
+# print("Shape of DataFrame:", data.shape)  # (rows, columns)
+# print("Size of DataFrame:", data.size)    # Total number of elements
 
 if isinstance(data, dict):
     for key, value in data.items():
@@ -18,9 +18,9 @@ if isinstance(data, dict):
 
 elif isinstance(data, list):
     print("Size:", len(data))
-    print(data[0])
-    # for index, item in enumerate(data):
-    #     print(f"Index: {index}, Item: {item}")
+    # print(data[0])
+    for index, item in enumerate(data[:10]):
+        print(f"Index: {index}, Item: {item}")
 
 elif isinstance(data, pd.DataFrame):
     print(data.head())
